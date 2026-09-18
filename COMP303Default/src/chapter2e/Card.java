f98dfd06f15431c38dd68f67dcddf3879123ff31
Yugo25 <mugiwara_yugo@icloud.com>
2026-09-18 12:36:37 -0400

    make chapter2e

---
+++ COMP303Default/src/chapter2e/Card.java
@@ -0,0 +1,12 @@
+package chapter2e;
+
+public class Card {
+    private boolean aIsRed; 
+    private boolean aIsSharp; // true if the suit shape has a sharp angle
+    private boolean aBi1;
+    private boolean aBi2;
+    private boolean aBi3;
+    private boolean aBi4;
+
+    public Card()
+}
